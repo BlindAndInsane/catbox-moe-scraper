58402fe2e74a5afe079b2444734e0dbbee6738b4
BlindAndInsane <blindandinsane@proton.me>
2024-10-12 07:51:46 +0530

    hmm fix

--- catbox/webhook.go
+++ catbox/webhook.go
@@ -1,46 +1,34 @@
 package catbox
 
 import (
-	"bytes"
-	"fmt"
-	"net/http"
-	"time"
+	"log"
+
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/webhook"
 )
 
 var (
-	httpClient = &http.Client{
-		Timeout: 5 * time.Second,
-	}
-	maxRetries = 3
-	retryDelay = 2 * time.Second
+	webhook_client webhook.Client
 )
 
+func init() {
+	client, err := webhook.NewWithURL(G_config.WebhookURL)
+	if err != nil {
+		G_logger.Errorln(err)
+	}
+	webhook_client = client
+}
+
 func SendMessageToWebhook(message string) error {
-	if G_config.WebhookURL == "" {
-		G_logger.Warn("Webhook URL is not configured; skipping webhook notification.")
-		return nil
+	msg, err := webhook_client.CreateMessage(discord.NewWebhookMessageCreateBuilder().
+		SetContent(message).
+		Build(),
+	)
+	if err != nil {
+		log.Printf("Failed to send webhook message: %v", err)
+		return err
 	}
 
-	payload := fmt.Sprintf(`{"content": "%s"}`, message)
-
-	var err error
-	for i := 0; i < maxRetries; i++ {
-		resp, err := httpClient.Post(G_config.WebhookURL, "application/json", bytes.NewBufferString(payload))
-		if err != nil {
-			G_logger.Warnf("Failed to send webhook: %v", err)
-		} else {
-			defer resp.Body.Close()
-			if resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusNoContent {
-				G_logger.Infof("Webhook message sent: %s", message)
-				return nil
-			} else {
-				err = fmt.Errorf("webhook returned status: %s", resp.Status)
-				G_logger.Warnf("Attempt %d: %s", i+1, err)
-			}
-		}
-
-		time.Sleep(retryDelay)
-	}
-
-	return err
+	log.Printf("Webhook message sent: %s", msg.Content)
+	return nil
 }
